9f9bbdce37c2dfce2e3563acd7204f923fd81c89
Sebastian Lucjan <sebastian.lucjan@gmail.com>
2021-03-12 12:26:30 +0100

    initial commit / base files / start cra

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./assets/seek-and-digest.png";
+import "./App.css";
+// import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Seek and Digest</h1>
+      <div className="App">test</div>
+    </>
+  );
+}
+
+export default App;
